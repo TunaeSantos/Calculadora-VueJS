89100b1a242d6c0d8387a27707860847ccfc9f3f
Tunae <tunae.santos82@gmail.com>
2024-06-20 14:45:24 -0300

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div >
+    <input v-model="numero1" type="number" placeholder="Número 1" />
+    <select v-model="operacao">
+      <option value="soma">+</option>
+      <option value="subtracao">-</option>
+      <option value="multiplicacao">*</option>
+      <option value="divisao">/</option>
+    </select>
+    <input v-model="numero2" type="number" placeholder="Número 2" />
+    <p>Resultado: {{ resultado }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: 'soma',
+    };
+  },
+  computed: {
+    resultado() {
+      switch (this.operacao) {
+        case 'soma':
+          return this.numero1 + this.numero2;
+        case 'subtracao':
+          return this.numero1 - this.numero2;
+        case 'multiplicacao':
+          return this.numero1 * this.numero2;
+        case 'divisao':
+          return this.numero1 / this.numero2;
+        default:
+          return 0;
+      }
+    },
+  },
+};
+</script>
